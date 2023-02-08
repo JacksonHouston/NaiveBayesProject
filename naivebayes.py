2b56e031931e3e54e49cd095a18df1d9c4a5a5ec
JacksonHouston <Houston.jack07@gmail.com>
2023-02-08 18:31:37 -0500

    testing output and reading the data

--- naivebayes.py
+++ naivebayes.py
@@ -6,22 +6,22 @@ totalMushrooms = len(D.instances) #get total number of mushrooms
 totalEdible = len(D.selectSubset({"class":"e"})) #get total number of edible mushrooms
 totalPoisonous = len(D.selectSubset({"class":"p"})) #get total number of poisonous mushrooms
 
-trainData = {} #initalize the dictionary for the training data
 m = 0 #virutal Sampling varible for testing
 keys = D.attributes.keys() #get all the keys for the attributes
 for key in keys:
     for item in D.getAttributeValues(key):
-        trainData.update({key: #nested dictionary to store the number of edible and poisonous for each item of each key
-            {item: 
-                {
-                    "Edible":len(D.selectSubset({"class":"e", key:item})), #number of edible per item per key
-                    "Poisonous":len(D.selectSubset({"class":"p", key:item})), #number of poisonous per item per key
-                    "p": 1/len(D.getAttributeValues(key)) #p 1/number of types of attributes per key (Prior probability of particular value)
-                }
-            }
-        })
+        print(key, item)
+        print("Edible", len(D.selectSubset({"class":"e", key:item}))) #number of edible per item per key
+        print("Poisonous",len(D.selectSubset({"class":"p", key:item}))) #number of poisonous per item per key
+        print("p", 1/len(D.getAttributeValues(key))) #p 1/number of types of attributes per key (Prior probability of particular value)
+        
 
 induction = {}
 
+def getFract(numItem, numTotal, m, p):
+    top = numItem + (m * p)
+    bottom = numTotal + m
+    return top/bottom
 
-print(trainData)
+#print(trainData['population'].keys())
+#print(trainData)
