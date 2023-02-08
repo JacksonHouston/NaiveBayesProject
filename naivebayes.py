059597c749574503af5c25e31b2059f01e9ae0a7
JacksonHouston <Houston.jack07@gmail.com>
2023-02-08 16:38:20 -0500

    Update naivebayes.py
    Added code to get occurrence of each attribute and store that into a dictionary

--- naivebayes.py
+++ naivebayes.py
@@ -2,19 +2,26 @@ import dataset
 
 D = dataset.Dataset("mushroom-training.data")
 
-totalMushrooms = len(D.instances)
-totalEdible = len(D.selectSubset({"class":"e"}))
-totalPoisonous = len(D.selectSubset({"class":"p"}))
-#print((totalEdible/totalMushrooms)*100, "'%' are edilbe")
-habitat = ["d","g","m","l","p","u","w"]
-classes = ["p","e"]
-totalHabEd=0
-totalHadPo=0
-for h in habitat:
-    for c in classes:
-        selectionCriteria = {"habitat":h, "class":c}
-        print("There are", len(D.selectSubset(selectionCriteria)), c ," examples in habitat" , h)
-        if c == "e":
-            totalHabEd += len(D.selectSubset(selectionCriteria))
-        else:
-            totalHadPo += len(D.selectSubset(selectionCriteria))
+totalMushrooms = len(D.instances) #get total number of mushrooms
+totalEdible = len(D.selectSubset({"class":"e"})) #get total number of edible mushrooms
+totalPoisonous = len(D.selectSubset({"class":"p"})) #get total number of poisonous mushrooms
+
+trainData = {} #initalize the dictionary for the training data
+m = 0 #virutal Sampling varible for testing
+keys = D.attributes.keys() #get all the keys for the attributes
+for key in keys:
+    for item in D.getAttributeValues(key):
+        trainData.update({key: #nested dictionary to store the number of edible and poisonous for each item of each key
+            {item: 
+                {
+                    "Edible":len(D.selectSubset({"class":"e", key:item})), #number of edible per item per key
+                    "Poisonous":len(D.selectSubset({"class":"p", key:item})), #number of poisonous per item per key
+                    "p": 1/len(D.getAttributeValues(key)) #p 1/number of types of attributes per key (Prior probability of particular value)
+                }
+            }
+        })
+
+induction = {}
+
+
+print(trainData)
