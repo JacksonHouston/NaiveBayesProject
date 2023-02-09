5454e269bb6c6c69842a3d21f5c2a123c9b4b028
JacksonHouston <Houston.jack07@gmail.com>
2023-02-08 19:07:22 -0500

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -6,15 +6,27 @@ totalMushrooms = len(D.instances) #get total number of mushrooms
 totalEdible = len(D.selectSubset({"class":"e"})) #get total number of edible mushrooms
 totalPoisonous = len(D.selectSubset({"class":"p"})) #get total number of poisonous mushrooms
 
+trainData = {}
+attributes = {}
+items = {}
 m = 0 #virutal Sampling varible for testing
 keys = D.attributes.keys() #get all the keys for the attributes
 for key in keys:
     for item in D.getAttributeValues(key):
-        print(key, item)
-        print("Edible", len(D.selectSubset({"class":"e", key:item}))) #number of edible per item per key
-        print("Poisonous",len(D.selectSubset({"class":"p", key:item}))) #number of poisonous per item per key
-        print("p", 1/len(D.getAttributeValues(key))) #p 1/number of types of attributes per key (Prior probability of particular value)
-        
+        items.update({
+            "Edible" : len(D.selectSubset({"class":"e", key:item})),
+            "Poisionous" : len(D.selectSubset({"class":"p", key:item})),
+            "p" : 1/len(D.getAttributeValues(key))
+            
+        })
+        #print(items)
+    attributes.update({item : items})
+    trainData.update({key: attributes})
+        # print(key, item)
+        # print("Edible", len(D.selectSubset({"class":"e", key:item}))) #number of edible per item per key
+        # print("Poisonous",len(D.selectSubset({"class":"p", key:item}))) #number of poisonous per item per key
+        # print("p", 1/len(D.getAttributeValues(key))) #p 1/number of types of attributes per key (Prior probability of particular value)
+print(trainData['habitat']['l'])
 
 induction = {}
 
