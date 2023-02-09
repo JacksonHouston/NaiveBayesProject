d1c94464eb79eb39c73b588d6c44c85200094c3f
JacksonHouston <Houston.jack07@gmail.com>
2023-02-08 22:33:23 -0500

    Update naivebayes.py
    updated the dictionaries after playing around with it more

--- naivebayes.py
+++ naivebayes.py
@@ -7,25 +7,23 @@ totalEdible = len(D.selectSubset({"class":"e"})) #get total number of edible mus
 totalPoisonous = len(D.selectSubset({"class":"p"})) #get total number of poisonous mushrooms
 
 trainData = {}
-attributes = {}
-items = {}
 m = 0 #virutal Sampling varible for testing
 keys = D.attributes.keys() #get all the keys for the attributes
 for key in keys:
+    trainData[key] = {}
     for item in D.getAttributeValues(key):
-        items.update({
+        trainData[key][item] = {
             "Edible" : len(D.selectSubset({"class":"e", key:item})),
             "Poisionous" : len(D.selectSubset({"class":"p", key:item})),
             "p" : 1/len(D.getAttributeValues(key))
-            
-        })
+        }
         #print(items)
-    attributes.update({item : items})
-    trainData.update({key: attributes})
         # print(key, item)
         # print("Edible", len(D.selectSubset({"class":"e", key:item}))) #number of edible per item per key
         # print("Poisonous",len(D.selectSubset({"class":"p", key:item}))) #number of poisonous per item per key
         # print("p", 1/len(D.getAttributeValues(key))) #p 1/number of types of attributes per key (Prior probability of particular value)
+
+print(trainData['habitat']['l']['Edible'])
 
 def getFract(numItem, numTotal, m, p):
     top = numItem + (m * p)
